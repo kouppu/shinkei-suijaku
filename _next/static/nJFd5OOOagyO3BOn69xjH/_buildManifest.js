187c43ff25453732dfb53dadaf7413762582d17c
suhrr <suhrr@users.noreply.github.com>
2022-05-21 01:37:46 +0000

    deploy: 097b592f1a945e1626e73e94423d59e02132679f

--- _next/static/nJFd5OOOagyO3BOn69xjH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/684-4ea32b7f519f8fed.js","static/chunks/pages/index-f42953303b57db62.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
